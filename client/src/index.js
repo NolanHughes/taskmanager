8b6d4ac5e0cf7759627defba17439d499c7f8457
NolanHughes <Nhughes987@gmail.com>
2018-12-18 17:28:19 -0600

    Delete importing index.css to index.js.

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './css/Index.css';
 import AppRouter from './components/AppRouter';
 import * as serviceWorker from './serviceWorker';
 
