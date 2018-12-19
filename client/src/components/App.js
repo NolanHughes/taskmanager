12a8d253e3caa237c66793c5bda19d6c90a7d9fb
NolanHughes <Nhughes987@gmail.com>
2018-12-18 18:47:02 -0600

    Trial for heroku deploy.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import Tasks from './Tasks';
-import Login from './Login'
-import Signup from './Signup'
+// import Login from './Login'
+// import Signup from './Signup'
 import '../css/App.css'
 
-export default (props) => {
-	return(
-		<Switch>			
-			<Route exact path="/" component={Tasks} />
-			<Route path="/login" component={Login} />
-			<Route path="/signup" component={Signup} />
-		</Switch>
-	)
+// export default (props) => {
+// 	return(
+// 		<Switch>			
+// 			<Route exact path="/" component={Tasks} />
+// 			<Route path="/login" component={Login} />
+// 			<Route path="/signup" component={Signup} />
+// 		</Switch>
+// 	)
+// }
+
+import NotFound from './NotFound'
+
+class App extends Component {
+  render () {
+    return <Router>
+      <Switch>
+        <Route path='/' exact component={Tasks} />
+
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
+  }
 }
+
+export default App
