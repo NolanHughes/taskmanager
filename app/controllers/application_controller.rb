ebdce5b2c1348d1fc2eb127b844cdd0d65df061c
NolanHughes <Nhughes987@gmail.com>
2018-12-18 19:22:23 -0600

    change application controller method.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,6 @@ class ApplicationController < ActionController::API
   end
 
   def fallback_index_html
-    render :file => '../../client/public/index.html'
+    render :file => 'public/index.html'
   end
 end
